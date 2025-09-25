33b09751a5457c059e34c09a4027ed4efdb9aebb
Jamie Bryson <jamieab98@gmail.com>
2025-09-24 20:17:08 -0400

    made the grid boxes interact-able

---
+++ TTTscript.js
@@ -0,0 +1,40 @@
+const box1 = document.getElementById("box1");
+const box2 = document.getElementById("box2");
+const box3 = document.getElementById("box3");
+const box4 = document.getElementById("box4");
+const box5 = document.getElementById("box5");
+const box6 = document.getElementById("box6");
+const box7 = document.getElementById("box7");
+const box8 = document.getElementById("box8");
+const box9 = document.getElementById("box9");
+const newGame = document.getElementById("newGame");
+const changeTheme = document.getElementById("changeTheme");
+
+
+box1.addEventListener("click", () => {
+    box1.textContent = "X";
+})
+box2.addEventListener("click", () => {
+    box2.textContent = "X";
+})
+box3.addEventListener("click", () => {
+    box3.textContent = "X";
+})
+box4.addEventListener("click", () => {
+    box4.textContent = "X";
+})
+box5.addEventListener("click", () => {
+    box5.textContent = "X";
+})
+box6.addEventListener("click", () => {
+    box6.textContent = "X";
+})
+box7.addEventListener("click", () => {
+    box7.textContent = "X";
+})
+box8.addEventListener("click", () => {
+    box8.textContent = "X";
+})
+box9.addEventListener("click", () => {
+    box9.textContent = "X";
+})
